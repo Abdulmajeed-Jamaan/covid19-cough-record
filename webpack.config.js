c3d9af83aa95c7db6d57164cc855d8ea6023aae8
Abdulmajeed <azzhraanee2011@hotmail.com>
2020-05-10 20:10:22 +0300

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
-        index: './js/index.js',
         'ios-support': './js/ios-support.js'
     },
     output: {
